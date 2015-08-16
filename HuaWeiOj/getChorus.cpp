f7d0e0121e57f1d4229893947a7b133c60c72120
FireFoxPlus <zhangzheming1990@hotmail.com>
2015-08-16 10:16:39 +0800

    ZZ

--- HuaWeiOj/getChorus.cpp
+++ HuaWeiOj/getChorus.cpp
@@ -6,32 +6,32 @@ int Chorus(vector<int> height){
     if(n == 0)
         return 0;
 
-    vector<int> left(n,1);
-    for(int i = 1;i < n;++i)
+    vector<int> left(n , 1);
+    for(int i = 1; i < n; ++i)
     {
-        for(int j = i-1;j >=0;--j)
+        for(int j = i - 1;  j >= 0; --j)
         {
             if(height[i] > height[j])
             {
-                if(left[i] < left[j]+1)
-                    left[i] = left[j]+1;
+                if(left[i] < left[j] + 1)
+                    left[i] = left[j] + 1;
             }
         }
     }
-    vector<int> right(n,1);
-    for(int i = n-2;i >= 0;--i)
+    vector<int> right(n , 1);
+    for(int i = n - 2; i >= 0; --i)
     {
-        for(int j = i+1;j < n;++j)
+        for(int j = i + 1; j < n; ++j)
         {
             if(height[i] > height[j])
             {
-                if(right[i] < right[j]+1)
-                    right[i] = right[j]+1;
+                if(right[i] < right[j] + 1)
+                    right[i] = right[j] + 1;
             }
         }
     }
     int maxs = 0;
-    for(int i = 0;i < n;++i)
+    for(int i = 0; i < n; ++i)
     {
         if(maxs < left[i] + right[i] - 1)
             maxs = left[i] + right[i] - 1;
